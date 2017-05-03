09feb6aa0a18b40431dbd6b72fbb05e994616027
Rich Inman <rinman24@gmail.com>
2017-05-03 12:42:50 -0700

    Initial commit
    Included README, __init__ for the chamber module, and a chamber.const
    package.

---
+++ README.rst
@@ -0,0 +1,27 @@
+====================
+UCSD Coimbra Chamber
+====================
+
+Table of Contents
+-----------------
+
+  * `Description`_
+  * `Repo Directory Structure`_.
+
+Description
+-----------
+
+Description will go here.
+
+Repo Directory Structure
+------------------------
+
+The structure of the directory::
+
+    ├── README.rst 
+	├── images
+	|   └── coimbra_ucsd_logo.png
+	├── chamber
+	|   ├── __init__.py
+	|   └── const.py
+	└── tests
---
+++ chamber/const.py
@@ -0,0 +1,23 @@
+"""Docstring."""
+
+# Stefan Tube Dimensions
+D_OUT_ST = 2.54e-2 # 2.54 cm
+R_OUT_ST = 1.27e-2 # 1.27 cm
+D_IN_ST = 2.36e-2 # 2.36 cm
+R_IN_ST = 1.18e-2 # 1.18 cm
+D_BASE_ST = 3.4e-2 # 3.4 cm
+R_BASE_ST = 1.7e-2 # 1.7 cm
+H_BASE_ST = 8e-4 # 0.8 mm
+H_TUBE_ST = 4.72e-2 # 4.72 cm
+A_C_ST = 4.37e-4 # 4.37 cm^2
+
+# Water Optical Properties at 10.59 microns
+K_ABS = 8.218e4 # 82,180 m^{-1}
+K_EXT = 6.925e-2 # 0.06925
+
+# Beam Parameters
+W = 8.3e-3 # 8.3 mm
+A_C_B = 2.16e-4 # 2.16 cm^2
+L_K_ABS = 1.22e-5 # 12 microns
+
+
