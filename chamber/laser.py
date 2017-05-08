0b0d0aaec14bee9b97677855bc26beb91e7ca8d2
Rich Inman <rinman24@gmail.com>
2017-05-08 11:35:55 -0700

    Unit test for laser.py
    Moved the calculation of the Rayleigh length to a dedicated function.
    Also included a unit test for this change.

--- chamber/laser.py
+++ chamber/laser.py
@@ -1,15 +1,25 @@
 """Docstring."""
 
+from math import pi
+
 class GaussianBeam(object):
     """GaussianBeam type contains methods related to Gaussian laser beams."""
     def __init__(self, lam=10.59e-6, power=20, radius=0.9e-3):
         """Use wavelength, power, and 1/e^2 waist radius to instanciate a Gaussian laser beam.
-        
+
         Keyword arguments:
         lam -- wavelength [m] of the laser (default 10.59e-6)
         radius -- 1/e^2 radius [m] of the beam (default 0.9e-3)
         power -- power [W] of the beam (default 20)
+
+        Returns:
+        GaussianBeam object
         """
         self.lam = lam
         self.power = power
         self.radius = radius
+        self.set_rayleigh()
+
+    def set_rayleigh(self):
+        """Use radius and wavelength to calculate Rayleigh length."""
+        self.rayleigh = pi * self.radius**2 / self.lam
--- tests/test_laser.py
+++ tests/test_laser.py
@@ -1,4 +1,5 @@
 import chamber.laser as laser
+from math import pi
 
 class TestLaser(object):
     """Unit testing of laser.py."""
@@ -9,6 +10,7 @@ class TestLaser(object):
         assert co2_laser.lam == 10.59e-6
         assert co2_laser.power == 20
         assert co2_laser.radius == 0.9e-3
+        print(co2_laser.radius)
 
     def test_laser_constructor_custom(self):
         """Check that a laser can be instanciated with a custom parameters."""
@@ -16,3 +18,8 @@ class TestLaser(object):
         assert co2_laser.lam == 635e-9
         assert co2_laser.power == 5e-3
         assert co2_laser.radius == 4e-3
+
+    def test_laser_constructor_rayleigh(self):
+        """Check that rayleigh length is calsulated correctly."""
+        co2_laser = laser.GaussianBeam()
+        assert co2_laser.rayleigh == pi* 0.9e-3**2 / 10.59e-6
