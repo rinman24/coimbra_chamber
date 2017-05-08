c76ab1d200f4bad8e574629b6367cd8104168f92
Rich Inman <rinman24@gmail.com>
2017-05-08 11:13:54 -0700

    Unit test for laser.py
    Added a unit test for the custom constructor of laser.py.

--- tests/test_laser.py
+++ tests/test_laser.py
@@ -9,3 +9,10 @@ class TestLaser(object):
         assert co2_laser.lam == 10.59e-6
         assert co2_laser.power == 20
         assert co2_laser.radius == 0.9e-3
+
+    def test_laser_constructor_custom(self):
+        """Check that a laser can be instanciated with a custom parameters."""
+        co2_laser = laser.GaussianBeam(lam=635e-9, power=5e-3, radius=4e-3)
+        assert co2_laser.lam == 635e-9
+        assert co2_laser.power == 5e-3
+        assert co2_laser.radius == 4e-3
