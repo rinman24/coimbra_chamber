d53ef08a10f07afe6f4da15e6e5538476585e977
Alex Leonelli <leonelliad@gmail.com>
2017-08-02 18:13:33 -0700

    resolved coments
    added new line at end, added global variabls

--- tests/test_data_transfer.py
+++ tests/test_data_transfer.py
@@ -1,17 +1,18 @@
 import pytest
 import chamber.data_transfer as data_transfer
 
+CORRECT_FILE_LIST = ["test.tdms", "unit_test_01.tdms", "unit_test_02.tdms",
+                     "unit_test_03.tdms", ".tdms"]
+INCORRECT_FILE_LIST = ["py.tdmstest", "py.tdmstest.py",
+                       "unit_test_01.tdms_index", "unit_test_02.tdms_index", "unit_test_03.tdms_index"]
+
 class TestSqlDb(object):
     """Unit testing of data_transfer.py."""
     
     def test_list_tdms(self):
         """Test correct output of all .tdms files contained in argument file."""
-        correct_file_list = ["test.tdms", "unit_test_01.tdms", "unit_test_02.tdms", 
-            "unit_test_03.tdms", ".tdms"]
-        incorrect_file_list = ["py.tdmstest", "py.tdmstest.py", 
-            "unit_test_01.tdms_index", "unit_test_02.tdms_index", "unit_test_03.tdms_index"]
         files = data_transfer.list_tdms("tests/data_transfer_test_files")
-        for file in incorrect_file_list:
+        for file in INCORRECT_FILE_LIST:
             assert file not in files
-        for file in correct_file_list:
+        for file in CORRECT_FILE_LIST:
             assert file in files
