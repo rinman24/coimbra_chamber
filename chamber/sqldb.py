de0281df9cd13e090be9a273e0a720b972af5e14
Alex Leonelli <leonelliad@gmail.com>
2017-08-05 01:21:11 -0700

    merged data_transfer.py and sqldb.py and relevant tests

--- chamber/sqldb.py
+++ chamber/sqldb.py
@@ -1,8 +1,10 @@
 """Docstring."""
-
 import os
+from re import compile
+
 import mysql.connector as conn
 from mysql.connector import errorcode
+from nptdms import TdmsFile
 
 def connect_sqldb():
     """Use connect constructor to connect to a MySQL server.
@@ -95,3 +97,51 @@ def setting_exists(cur, setting):
         return False
     else:
         return result[0][0]
+
+def list_tdms(file_path):
+    """returns a list of the .tdms files contained within the argument file."""
+    regex = compile(".tdms")
+    file_list = [file for file in os.listdir(file_path) if regex.match(file, len(file)-5)]
+    return file_list
+
+def get_settings(tdms_obj):
+    """returns a dictionary of the initial state of Tests in the TdmsFile object argument"""
+    settings = {'initial_dew_point': tdms_obj.object("Data", "DewPoint").data[0],
+        'initial_duty': tdms_obj.object("Data", "DutyCycle").data[0],
+        'initial_mass': tdms_obj.object("Data", "Mass").data[0],
+        'initial_pressure': tdms_obj.object("Data", "Pressure").data[0],
+        'initial_temp': sum(tdms_obj.object("Data", "TC{}".format(x)).data[0] for x in range(3,14))/11
+        }
+    return settings
+
+def get_tests(tdms_obj):
+    """returns a dictionary of the initial state of Settings in the TdmsFile object argument"""
+    tests = { 'author': "", 'date_time': tdms_obj.object().properties['DateTime'],
+        'description': "", 'time_step': tdms_obj.object("Settings", "TimeStep").data[0]
+        }
+
+    for name, value in tdms_obj.object().properties.items():
+        if name == "author":
+            tests['author'] = value
+        if name == "description":
+            tests['description'] = value
+    return tests
+
+def get_obs(tdms_obj, idx):
+    """returns a dictionary of strings derived from tdms object observation data"""
+    observations = {'cap_man_ok': str(tdms_obj.object("Data", "CapManOk").data[idx]),
+        'dew_point': str(tdms_obj.object("Data", "DewPoint").data[idx]),
+        'duty_cycle': str(tdms_obj.object("Data", "DutyCycle").data[idx]),
+        'idx': str(tdms_obj.object("Data", "Idx").data[idx]),
+        'mass': str(tdms_obj.object("Data", "Mass").data[idx]),
+        'optidew_ok': str(tdms_obj.object("Data", "OptidewOk").data[idx]),
+        'pow_out': str(tdms_obj.object("Data", "PowOut").data[idx]),
+        'pow_ref': str(tdms_obj.object("Data", "PowRef").data[idx]),
+        'pressure': str(tdms_obj.object("Data", "Pressure").data[idx]),
+        }
+    return observations
+
+def get_temp(tdms_obj, idx):
+    """returns a dictionary of strings derived from tdms object temperature data"""
+    temp = {'TC_num': str(idx), 'temp': str(tdms_obj.object("Data", "TC{}".format(idx)).data[idx])}
+    return temp
--- tests/test_sqldb.py
+++ tests/test_sqldb.py
@@ -1,8 +1,12 @@
 """Docstring."""
+from datetime import datetime
 from math import isclose
+
 import mysql.connector as conn
 from mysql.connector import errorcode
+from nptdms import TdmsFile
 import pytest
+import pytz
 
 import chamber.sqldb as sqldb
 
@@ -22,6 +26,29 @@ SETTINGS_1 = {'InitialDewPoint': '100', 'InitialDuty': '100', 'InitialMass': '0.
               'InitialPressure': '100000', 'InitialTemp': '290', 'TimeStep': '1'}
 SETTINGS_2 = {'InitialDewPoint': '500', 'InitialDuty': '1000', 'InitialMass': '20',
               'InitialPressure': '8', 'InitialTemp': '400', 'TimeStep': '20'}
+CORRECT_FILE_LIST = ["test.tdms", "unit_test_01.tdms", "unit_test_02.tdms",
+    "unit_test_03.tdms"
+    ]
+INCORRECT_FILE_LIST = ["py.tdmstest", "py.tdmstest.py", "unit_test_01.tdms_index",
+    "unit_test_02.tdms_index", "unit_test_03.tdms_index"
+    ]
+TDMS_TEST_FILES = ["tests/data_transfer_test_files/tdms_test_files/tdms_test_file_01.tdms",
+    "tests/data_transfer_test_files/tdms_test_files/tdms_test_file_02.tdms",
+    "tests/data_transfer_test_files/tdms_test_files/tdms_test_file_03.tdms"
+    ]
+TDMS_01_DICT_SETS = {'initial_dew_point': 292.501, 'initial_duty': 0, 'initial_mass': -0.0658138,
+    'initial_pressure': 99977, 'initial_temp': (297.302+297.27
+    +297.284+296.835+296.753+297.094+297.054+296.928+296.86+297.318+297.325)/11
+    }
+TDMS_01_DICT_TESTS = {'author': "ADL", 'date_time': datetime(2017, 8, 3, 19, 33, 9, 217290, pytz.UTC),
+    'description': "This is at room temperature, pressure, no laser power, study of boundy development.",
+    'time_step': 1
+    }
+TDMS_01_THM_07 = {'TC_num': "7", 'temp': "296.762"}
+TDMS_01_OBS_08 = {'cap_man_ok': "1.0", 'dew_point': "292.427", 'duty_cycle': "0.0", 'idx': "8.0",
+    'mass': "-0.0658138", 'optidew_ok': "1.0", 'pow_out': "-0.001", 'pow_ref': "-0.0015", 'pressure': "99982.0"
+    }
+TEST_INDEX = 7
 
 @pytest.fixture(scope='module')
 def cursor():
@@ -38,6 +65,12 @@ def cursor():
     cur.close()
     cnx.close()
     print("Connection to MySQL closed.")
+
+@pytest.fixture(scope='module')
+def test_tdms_obj():
+    """fixture to instantiate only one TdmsFile object for testing"""
+    return TdmsFile(TDMS_TEST_FILES[0])
+
 
 class TestSqlDb(object):
     """Unit testing of sqldb.py."""
@@ -75,3 +108,32 @@ class TestSqlDb(object):
         assert not sqldb.setting_exists(cursor, SETTINGS_2)
         setting_id = sqldb.last_insert_id(cursor)
         cursor.execute("DELETE FROM Setting WHERE SettingID = {};".format(setting_id))
+
+    def test_list_tdms(self):
+        """Test correct output of all .tdms files contained in argument file."""
+        files = sqldb.list_tdms("tests/data_transfer_test_files")
+
+        for file in INCORRECT_FILE_LIST:
+            assert file not in files
+        for file in CORRECT_FILE_LIST:
+            assert file in files
+
+    def test_get_settings(self, test_tdms_obj):
+        """Test correct dictionary output when reading .tdms files for settings"""
+        assert TDMS_01_DICT_SETS == sqldb.get_settings(test_tdms_obj)
+
+    def test_get_tests(self, test_tdms_obj):
+        """Test correct dictionary output when reading .tdms files for tests"""
+        assert TDMS_01_DICT_TESTS == sqldb.get_tests(test_tdms_obj)
+    
+    def test_fixture(self, test_tdms_obj):
+        """Test existence of test_tdms_obj fixture"""
+        assert test_tdms_obj
+
+    def test_get_temp(self, test_tdms_obj):
+        """Test correcct output when converting tdms obj temperature data to dictionary of strings"""
+        assert TDMS_01_THM_07 == sqldb.get_temp(test_tdms_obj, TEST_INDEX)
+
+    def test_get_obs(self, test_tdms_obj):
+        """Test correcct output when converting tdms obj observation data to dictionary of strings"""
+        assert TDMS_01_OBS_08 == sqldb.get_obs(test_tdms_obj, TEST_INDEX)
