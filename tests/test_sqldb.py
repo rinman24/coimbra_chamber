3bf08b0b7d6cb918dae7b19703a6ff88abd3ab65
Rich Inman <rinman24@gmail.com>
2017-08-17 14:13:10 -0700

    Added SQL transactions for each file.
    This should speed up performance quite a bit.

--- tests/test_sqldb.py
+++ tests/test_sqldb.py
@@ -55,20 +55,25 @@ TEST_DICTIONARY = {'InitialDewPoint': '292.50', 'InitialDuty': '0.0', 'InitialMa
 TEST_INDEX = 7
 
 @pytest.fixture(scope='module')
-def cursor():
-    """Cursor Fixture at module level so that only one connection is made."""
+def cnx_cur():
+    """Cnx and cur Fixture at module level so that only one connection is made."""
     print("\nConnecting to MySQL...")
     cnx = sqldb.connect_sqldb()
+    cnx.autocommit = False
     cur = cnx.cursor()
     print("Connected.")
-    yield cur
+    yield cnx, cur
     print("\nCleaning up test database...")
     cur.execute("DROP TABLE UnitTest;")
-    print("Disconnecting from MySQL...")
+    cur.execute("DELETE FROM TempObservation;")
+    cur.execute("DELETE FROM Observation;")
+    cur.execute("DELETE FROM Test;") 
+    cur.execute("DELETE FROM Setting;")
     cnx.commit()
+    print('Disconnecting from MySQL...')
     cur.close()
     cnx.close()
-    print("Connection to MySQL closed.")
+    print('Connection to MySQL closed.')
 
 @pytest.fixture(scope='module')
 def test_tdms_obj():
@@ -79,15 +84,19 @@ def test_tdms_obj():
 class TestSqlDb(object):
     """Unit testing of sqldb.py."""
 
-    def test_connection(self, cursor):
+    def test_connection(self, cnx_cur):
         """Test connection to the MySQL database."""
-        assert cursor
+        cnx = cnx_cur[0]
+        cur = cnx_cur[1]
+        assert cnx_cur[0]
+        assert cnx_cur[1]
 
-    def test_create_table(self, cursor):
+    def test_create_table(self, cnx_cur):
         """"Test DDL for table creation."""
-        sqldb.create_tables(cursor, TABLES)
-        cursor.execute("SELECT 1 FROM UnitTest LIMIT 1;")
-        assert len(cursor.fetchall()) == 0
+        cur = cnx_cur[1]
+        sqldb.create_tables(cur, TABLES)
+        cur.execute("SELECT 1 FROM UnitTest LIMIT 1;")
+        assert len(cur.fetchall()) == 0
 
     def test_build_insert_dml(self):
         """Test DML for INSERT statements."""
@@ -95,19 +104,21 @@ class TestSqlDb(object):
         ref = "INSERT INTO UnitTest     (String)  VALUES    ('unit testing');"
         assert ref == query
 
-    def test_enter_into_table(self, cursor):
+    def test_enter_into_table(self, cnx_cur):
         """Test DDL for row insertion."""
-        cursor.execute(sqldb.insert_dml('UnitTest', ROW_DATA_2))
-        cursor.execute("SELECT Number FROM UnitTest WHERE String = 'more testing';")
-        assert isclose(float(cursor.fetchall()[0][0]), 99.9)
+        cur = cnx_cur[1]
+        cur.execute(sqldb.insert_dml('UnitTest', ROW_DATA_2))
+        cur.execute("SELECT Number FROM UnitTest WHERE String = 'more testing';")
+        assert isclose(float(cur.fetchall()[0][0]), 99.9)
 
-    def test_setting_exists(self, cursor):
+    def test_setting_exists(self, cnx_cur):
         """Test that you can find settings that already exist."""
-        cursor.execute(sqldb.insert_dml('Setting', SETTINGS_1))
-        assert sqldb.setting_exists(cursor, SETTINGS_1)
-        assert not sqldb.setting_exists(cursor, SETTINGS_2)
-        setting_id = cursor.lastrowid
-        cursor.execute("DELETE FROM Setting WHERE SettingID = {};".format(setting_id))
+        cur = cnx_cur[1]
+        cur.execute(sqldb.insert_dml('Setting', SETTINGS_1))
+        assert sqldb.setting_exists(cur, SETTINGS_1)
+        assert not sqldb.setting_exists(cur, SETTINGS_2)
+        setting_id = cur.lastrowid
+        cur.execute("DELETE FROM Setting WHERE SettingID = {};".format(setting_id))
 
     def test_list_tdms(self):
         """Test correct output of all .tdms files contained in argument file."""
@@ -138,48 +149,36 @@ class TestSqlDb(object):
         """Test correct output when converting tdms obj observation data to dictionary of strings."""
         assert TDMS_01_OBS_08 == sqldb.get_obs(test_tdms_obj, TEST_INDEX)
 
-    def test_add_input(self, cursor):
+    def test_add_input(self, cnx_cur):
         """Test correct data insertion through checking add_temp, the final table in cascade."""
-        clear_sqldb(cursor)
-        sqldb.add_input(cursor, TEST_DIRECTORY)
-        cursor.execute("Select Temperature FROM TempObservation WHERE TempObservationID = '{}'".format(cursor.lastrowid))
-        res = cursor.fetchone()
-        print(type(res), len(res), type(res[0]), res[0])
+        cnx = cnx_cur[0]
+        cur = cnx_cur[1]
+        sqldb.add_input(cnx, cur, TEST_DIRECTORY)
+        cur.execute("Select Temperature FROM TempObservation WHERE TempObservationID = '{}'".format(cur.lastrowid))
+        res = cur.fetchone()
         assert isclose(float(res[0]), 297.23)
-        clear_sqldb(cursor)
 
-    def test_add_setting(self, cursor, test_tdms_obj):
+    def test_add_setting(self, cnx_cur, test_tdms_obj):
         """Test correct data insertion and condition handling in add_setting."""
-        clear_sqldb(cursor)
-        sqldb.add_setting(cursor, test_tdms_obj)
-        setting_id = sqldb.setting_exists(cursor, sqldb.get_settings(test_tdms_obj))
+        cur = cnx_cur[1]
+        sqldb.add_setting(cur, test_tdms_obj)
+        setting_id = sqldb.setting_exists(cur, sqldb.get_settings(test_tdms_obj))
         assert isinstance(setting_id, int)
-        assert sqldb.add_setting(cursor, test_tdms_obj) == setting_id
-        cursor.execute('DELETE FROM Setting;')
+        assert sqldb.add_setting(cur, test_tdms_obj) == setting_id
 
-    def test_add_test(self, cursor, test_tdms_obj):
+    def test_add_test(self, cnx_cur, test_tdms_obj):
         """Test correct data insertion and condition handling in add_test."""
-        clear_sqldb(cursor)
-        setting_id = sqldb.add_setting(cursor, test_tdms_obj)
-        sqldb.add_test(cursor, test_tdms_obj, str(setting_id))
-        cursor.execute("Select Author FROM Test WHERE TestID = '{}'".format(cursor.lastrowid))
-        assert cursor.fetchone()[0] == "ADL"
-        clear_sqldb(cursor)
+        cur = cnx_cur[1]
+        setting_id = sqldb.add_setting(cur, test_tdms_obj)
+        sqldb.add_test(cur, test_tdms_obj, str(setting_id))
+        cur.execute("Select Author FROM Test WHERE TestID = '{}'".format(cur.lastrowid))
+        assert cur.fetchone()[0] == "ADL"
 
-    def test_add_obs(self, cursor, test_tdms_obj):
+    def test_add_obs(self, cnx_cur, test_tdms_obj):
         """Test correct data insertion and condition handling in add_obs."""
-        clear_sqldb(cursor)
-        test_id = sqldb.add_test(cursor, test_tdms_obj, str(sqldb.add_setting(cursor, test_tdms_obj)))
+        cur = cnx_cur[1]
+        test_id = sqldb.add_test(cur, test_tdms_obj, str(sqldb.add_setting(cur, test_tdms_obj)))
         for obs_idx in range(len(test_tdms_obj.object("Data", "Idx").data)):
-            sqldb.add_obs(cursor, test_tdms_obj, str(test_id), obs_idx)
-        cursor.execute("Select Duty FROM Observation WHERE ObservationID = '{}'".format(cursor.lastrowid))
-        assert isclose(float(cursor.fetchone()[0]), 0)
-        clear_sqldb(cursor)
-
-
-def clear_sqldb(cursor):
-    """Clears test database."""
-    cursor.execute('DELETE FROM TempObservation;')
-    cursor.execute('DELETE FROM Observation;')
-    cursor.execute('DELETE FROM Test;') 
-    cursor.execute('DELETE FROM Setting;')
+            sqldb.add_obs(cur, test_tdms_obj, str(test_id), obs_idx)
+        cur.execute("Select Duty FROM Observation WHERE ObservationID = '{}'".format(cur.lastrowid))
+        assert isclose(float(cur.fetchone()[0]), 0)
