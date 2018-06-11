6509b7abed048541fc8f60ba64346088d10378b5
Rich Inman <rinman24@gmail.com>
2018-06-11 11:27:46 -0700

    Final clean up before merge with master.

--- tests/models/test_params.py
+++ tests/models/test_params.py
@@ -12,11 +12,11 @@ TDP_VALUE = 280
 
 
 def test_get_schmidt():
-	assert math.isclose(
-        params.get_schmidt(P_VALUE, T_VALUE, TDP_VALUE, 'Mills'),
-        0.6104149007992397
-        )
-	assert math.isclose(
-        params.get_schmidt(P_VALUE, T_VALUE, TDP_VALUE, 'Marrero'),
-        0.6272163625320014 
-        )
+    assert math.isclose(
+            params.get_schmidt(P_VALUE, T_VALUE, TDP_VALUE, 'Mills'),
+            0.6104149007992397
+            )
+    assert math.isclose(
+            params.get_schmidt(P_VALUE, T_VALUE, TDP_VALUE, 'Marrero'),
+            0.6272163625320014
+            )
