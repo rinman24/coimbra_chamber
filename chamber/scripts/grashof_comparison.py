1029f5fe3dc46ac2521fcf332e7a05886a6d312d
Alex Leonelli <leonelliad@gmail.com>
2018-07-02 10:45:10 -0700

    fixes order of arguments in grashof call

--- chamber/scripts/grashof_comparison.py
+++ chamber/scripts/grashof_comparison.py
@@ -12,13 +12,13 @@ def range_analysis():
     df = pd.DataFrame()
     for p in range(int(3e4), int(1.01325e5), int(1e4)):
         # Uses the model limit as the upper bound of the loop range
-        for t_e in range(275, int(310*pow(p/101325, 0.09)), 5):
-            for t_s in (t_e_int/2 for t_e_int in range(2 * t_e, 2 * t_e + 5)):
+        for t in range(275, int(310*pow(p/101325, 0.09)), 5):
+            for t_s in (t_int/2 for t_int in range(2 * t, 2 * t + 5)):
                 for rh in (rh_int/100 for rh_int in range(5, 90, 5)):
-                    t_dp = props.get_tdp(p, t_e, rh)
-                    grashof = params.get_grashof(p, t_e, t_s, t_dp)
+                    t_dp = props.get_tdp(p, t, rh)
+                    grashof = params.get_grashof(p, t, t_dp, t_s)
                     df = df.append({
-                        'Pressure (Pa)': p, 'Environment Temp (K)': t_e,
+                        'Pressure (Pa)': p, 'Environment Temp (K)': t,
                         'Surface Temp (K)': t_s, 'Dew Point (K)': t_dp,
                         'Ralative Humidity': rh, 'Grashof': grashof
                         }, ignore_index=True)
