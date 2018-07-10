7300166f7ad05423682d7bef81a5220baffdc3d3
Alex Leonelli <leonelliad@gmail.com>
2018-07-10 13:17:25 -0700

    Reformats sqldb module level docstring

--- chamber/data/sqldb.py
+++ chamber/data/sqldb.py
@@ -3,14 +3,12 @@ MySQL and TDMS Integration.
 
 Functions
 ---------
-    add_tdms_file
-    add_tube_info
-    connect
-    create_tables
+- `add_tdms_file` -- Insert tdms files into the MySQL database.
+- `add_tube_info` -- Add test-independant Tube information.
+- `connect` -- Get a connection and cursor to a MySQL database.
+- `create_tables` -- Create tables in the database.
 
-To Do
------
-    Decouple database and tdms volatility via modulde encapsulation.
+.. todo:: Decouple database and tdms volatility via modulde encapsulation.
 """
 import configparser
 import re
