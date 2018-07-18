4a44db9f3abee3c894410c59427dbcba87bbc3b8
Alex Leonelli <leonelliad@gmail.com>
2018-07-18 16:26:46 -0700

    adds a test setup.py file

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='Chmaber',
+    version='0.1.0',
+    author='Rich H. Inman',
+    author_email='rinman24@gmail.com',
+    packages=['chamber', 'chamber.data', 'chamber.models',
+              'chamber.tools', 'chamber.analysis'],
+    url='https://github.com/rinman24/chamber',
+    license='LICENSE.txt',
+    description=('Python programs for Rich Inman\'s PhD work involving the the'
+                 'Coimbra Chamber at UCSD. '),
+    long_description=open('README.txt').read(),
+    # install_requires=[
+    # "nptdms >= 1.1.1",
+    # "test2 == 0.1.4",
+    # ],
+)
