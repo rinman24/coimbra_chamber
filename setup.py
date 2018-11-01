ccb0b7d8447fca8e94587fd636703fda954187ab
Rich Inman <rinman24@gmail.com>
2018-11-01 12:57:22 -0700

    Removes comma.

--- setup.py
+++ setup.py
@@ -14,16 +14,5 @@ setup(
         'Python programs for Rich Inman\'s PhD work involving the Coimbra '
         'Chamber at UCSD.'
         ),
-    long_description=open('README.rst').read(),
-    install_requires=[
-        'npTDMS==0.11.3',
-        'matplotlib==2.1.0',
-        'tabulate==0.8.1',
-        'scipy==0.19.1',
-        'numpy==1.13.3',
-        'pandas==0.22.0',
-        'CoolProp==6.1.0',
-        'mysql-connector=2.1.6',
-        'schedule==0.5.0',
-        ],
+    long_description=open('README.rst').read()
     )
