37d79e999405374d3ba2530de3cd920fd1aed0b9
Rich Inman <rinman24@gmail.com>
2018-11-01 13:49:49 -0700

    Adds @staticmethod decorator to unit tests.

--- tests/access/experiment_access_unit_test.py
+++ tests/access/experiment_access_unit_test.py
@@ -13,31 +13,27 @@ _DB_CREDENTIALS = dict(
 
 
 @pytest.fixture
-def mock_mysql_connector(monkeypatch):  # noqa: D103
-    print('mock_mysql_connector!!!!!!')
+def mock_mysql_connector(monkeypatch):
+    """Avoid call to external mySQL database."""
     mock_mysql_connector = mock.MagicMock()
     mock_mysql_connector.connect = mock.MagicMock(return_value='cnx')
+
     monkeypatch.setattr(
         'mysql.connector.connect', mock_mysql_connector.connect
         )
+
     return mock_mysql_connector
 
 
 class TestConnect(object):
     """Unit test suite for chamber.access.experiment.connect()."""
 
-    def test_can_call_connect(
-            self, mock_mysql_connector, monkeypatch
-            ):  # noqa: D102
-
+    @staticmethod
+    def test_can_call_connect(mock_mysql_connector, monkeypatch):  # noqa: D102
         exp_acc.connect(**_DB_CREDENTIALS)
-
         mock_mysql_connector.connect.assert_called_with(**_DB_CREDENTIALS)
 
-    def test_connect_returns_cnx(
-            self, mock_mysql_connector, monkeypatch
-            ):  # noqa: D102
-
+    @staticmethod
+    def test_connect_returns_cnx(mock_mysql_connector, monkeypatch):  # noqa: D102
         result = exp_acc.connect(**_DB_CREDENTIALS)
-
         assert result == 'cnx'
