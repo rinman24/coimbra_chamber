f7bea8bd9e8ab5b370a59ab669534193f15ae2e3
Rich Inman <rinman24@gmail.com>
2018-11-01 13:44:30 -0700

    Adds refactored connect tests.

--- tests/access/experiment_access_unit_test.py
+++ tests/access/experiment_access_unit_test.py
@@ -4,21 +4,40 @@ import unittest.mock as mock
 
 import pytest
 
-import chamber.access.experiment as exp
+import chamber.access.experiment as exp_acc
 
 
-def test_connect_returns_cnx(monkeypatch):  # noqa D103
+_DB_CREDENTIALS = dict(
+    host='host', user='user', password='password', database='database'
+    )
+
+
+@pytest.fixture
+def mock_mysql_connector(monkeypatch):  # noqa: D103
+    print('mock_mysql_connector!!!!!!')
     mock_mysql_connector = mock.MagicMock()
     mock_mysql_connector.connect = mock.MagicMock(return_value='cnx')
     monkeypatch.setattr(
         'mysql.connector.connect', mock_mysql_connector.connect
         )
-    config = dict(
-        host='host',
-        user='user',
-        password='password',
-        database='database'
-        )
-    result = exp.connect(**config)
-    mock_mysql_connector.connect.assert_called_with(**config)
-    assert result == 'cnx'
+    return mock_mysql_connector
+
+
+class TestConnect(object):
+    """Unit test suite for chamber.access.experiment.connect()."""
+
+    def test_can_call_connect(
+            self, mock_mysql_connector, monkeypatch
+            ):  # noqa: D102
+
+        exp_acc.connect(**_DB_CREDENTIALS)
+
+        mock_mysql_connector.connect.assert_called_with(**_DB_CREDENTIALS)
+
+    def test_connect_returns_cnx(
+            self, mock_mysql_connector, monkeypatch
+            ):  # noqa: D102
+
+        result = exp_acc.connect(**_DB_CREDENTIALS)
+
+        assert result == 'cnx'
