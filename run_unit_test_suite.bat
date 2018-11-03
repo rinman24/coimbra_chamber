2f5bc32f9e7a762596f3a16c78b225df3a1620cc
Rich Inman <rinman24@gmail.com>
2018-11-03 14:42:51 -0700

    Moves v 1.0 into depreciated folders.

--- run_unit_test_suite.bat
+++ run_unit_test_suite.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Running unit test suite...
-python -m pytest -k "unit" -xvs --cov-report html --cov=chamber
+python -m pytest -k "unit" -xvs --cov-report html --cov=chamber --ignore=tests_depreciated\
 echo Testing complete.
  
--- tests/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Test suite __init__."""
