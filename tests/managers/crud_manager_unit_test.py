a79cb856ae8cf48127ba6d8ce1d5121608574775
Rich Inman <rinman24@gmail.com>
2018-11-03 11:19:20 -0700

    Adds correct output tests to get_credentials.

--- tests/managers/crud_manager_unit_test.py
+++ tests/managers/crud_manager_unit_test.py
@@ -7,18 +7,17 @@ import pytest
 import chamber.managers.crud as crud_mngr
 
 
-def test_can_call_get_creds(monkeypatch):  # noqa: D103
+@pytest.fixture
+def mock_config(monkeypatch):
+    """Mock of an instance of the configparser.ConfigParser() class."""
     mock_config = mock.MagicMock()
     mock_config.read = mock.MagicMock()
     monkeypatch.setattr(
         'configparser.ConfigParser.read', mock_config.read
         )
 
-    mock_ConfigParser = mock.MagicMock(return_value=mock_config)
-    monkeypatch.setattr('configparser.ConfigParser', mock_ConfigParser)
-
-    # The following lines are required for the mock of config['MySQL-Server']
-    # section to behave properly in the dict(config['MySQL-Server']) call.
+    # Production code calls Python builtin dict() on
+    # mock_config['MySQL-Server'].
     mock_config['MySQL-Server'] = mock.MagicMock()
     mock_config['MySQL-Server'].keys.return_value.__iter__.return_value = [
         'host', 'user', 'password'
@@ -27,31 +26,24 @@ def test_can_call_get_creds(monkeypatch):  # noqa: D103
         'test_host', 'test_user', 'test_password'
         ]
 
+    return mock_config
+
+
+@pytest.fixture
+def mock_ConfigParser(mock_config, monkeypatch):
+    """Mock of the configparser.ConfigParser() constructor."""
+    mock_ConfigParser = mock.MagicMock(return_value=mock_config)
+    monkeypatch.setattr('configparser.ConfigParser', mock_ConfigParser)
+    return mock_ConfigParser
+
+
+def test_can_call_get_creds(mock_ConfigParser, mock_config):  # noqa: D103
     crud_mngr.get_credentials('test_database')
 
     mock_config.read.assert_called_once_with('config.ini')
 
 
-def test_get_creds_returns_correct_dict(monkeypatch):  # noqa: D103
-    mock_config = mock.MagicMock()
-    mock_config.read = mock.MagicMock()
-    monkeypatch.setattr(
-        'configparser.ConfigParser.read', mock_config.read
-        )
-
-    mock_ConfigParser = mock.MagicMock(return_value=mock_config)
-    monkeypatch.setattr('configparser.ConfigParser', mock_ConfigParser)
-
-    # The following lines are required for the mock of config['MySQL-Server']
-    # section to behave properly in the dict(config['MySQL-Server']) call.
-    mock_config['MySQL-Server'] = mock.MagicMock()
-    mock_config['MySQL-Server'].keys.return_value.__iter__.return_value = [
-        'host', 'user', 'password'
-        ]
-    mock_config['MySQL-Server'].__getitem__.side_effect = [
-        'test_host', 'test_user', 'test_password'
-        ]
-
+def test_get_creds_returns_correct_dict(mock_ConfigParser, mock_config):  # noqa: D103
     creds = crud_mngr.get_credentials('test_database')
 
     correct_creds = dict(
@@ -59,5 +51,3 @@ def test_get_creds_returns_correct_dict(monkeypatch):  # noqa: D103
         database='test_database'
         )
     assert creds == correct_creds
-
-# tests are passing, just refactor the duplication into fixtures
