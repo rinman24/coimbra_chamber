78c74d57c23947469d9d7a08e970b91ff2d1dff0
Rich Inman <rinman24@gmail.com>
2018-11-03 17:52:52 -0700

    Adds try statemet for tables that already exist.

--- chamber/access/experiment.py
+++ chamber/access/experiment.py
@@ -1,10 +1,20 @@
 """Experiment access module."""
 
 import chamber.utilities.ddl as ddl
+import mysql.connector
 
 
 def build_experiment_tables(cursor):
     for table in ddl.build_instructions['experiments', 'table order']:
-        print('Building table: {}...'.format(table), end=' ')
-        cursor.execute(ddl.build_instructions['experiments', 'ddl'][table])
-        print('done')
+        try:
+            print('Creating table {}: '.format(table), end='')
+            cursor.execute(
+               ddl.build_instructions['experiments', 'ddl'][table]
+               )
+        except mysql.connector.Error as err:
+            if err.errno == mysql.connector.errorcode.ER_TABLE_EXISTS_ERROR:
+                print('already exists.')
+            else:
+                print(err.msg)
+        else:
+            print('OK')
