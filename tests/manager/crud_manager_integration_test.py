c960f5eee83c7796b0bc997f903071ebea670970
Rich Inman <rinman24@gmail.com>
2018-11-05 22:47:56 -0800

    Updates integration tests.

--- tests/manager/crud_manager_integration_test.py
+++ tests/manager/crud_manager_integration_test.py
@@ -3,9 +3,9 @@
 import chamber.manager.crud as crud_mngr
 
 # ----------------------------------------------------------------------------
-# setup_experiment_tables
+# build_tables
 
 
 def test_setup_experiment_tables_returns_success():  # noqa: D103
-    message = crud_mngr.setup_experiment_tables('schema')
-    assert message == 'Success.'
+    message = crud_mngr.build_tables('schema')
+    assert message == 'Success.' 
