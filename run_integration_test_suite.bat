7afb64359c23b0f667130a02d13285e05c883340
Rich Inman <rinman24@gmail.com>
2018-11-06 21:30:30 -0800

    Adds drop to integration tests.

--- run_integration_test_suite.bat
+++ run_integration_test_suite.bat
@@ -1,5 +1,5 @@
 @echo off
-echo Running unit test suite...
+echo Running integration test suite...
 python -m pytest -k "integration" -xvs --cov-report html --cov=chamber --ignore=tests_depreciated\
 echo Testing complete.
  
--- tests/manager/crud_manager_integration_test.py
+++ tests/manager/crud_manager_integration_test.py
@@ -9,3 +9,10 @@ import chamber.manager.crud as crud_mngr
 def test_can_create_experiment_tables():  # noqa: D103
     message = crud_mngr.create_tables('experiment')
     assert message == 'Successfully built experiment tables.'
+
+
+def test_can_drop_experiment_tables():  # noqa: D103
+    message = crud_mngr.drop_tables('experiment', drop_db=True)
+    assert message == (
+        'Successfully dropped experiment tables. Database also dropped.'
+        )
