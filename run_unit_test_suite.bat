915d23514a0bd1e84521a24d5f6c780d436987aa
Rich Inman <rinman24@gmail.com>
2018-11-18 14:00:29 -0800

    Adds debugger to tests suite.

--- run_unit_test_suite.bat
+++ run_unit_test_suite.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Running unit test suite...
-python -m pytest -k "unit" -xvs --cov-report html --cov=chamber --ignore=tests_depreciated\
+python -m pytest -k "unit" -xvs --cov-report html --cov=chamber --ignore=tests_depreciated\ --pdb
 echo Testing complete.
  
