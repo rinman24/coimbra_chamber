3359f458961981828c5b72c5861d21eedf2e8a00
Rich Inman <rinman24@gmail.com>
2019-01-15 19:52:08 -0800

    Ignore warnings in integration tests.

--- run_integration_test_suite.bat
+++ run_integration_test_suite.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Running integration test suite...
-python -m pytest -k "integration" -vs --cov-report html --cov=chamber --ignore=tests_depreciated\
+python -m pytest -k "integration" -vs --cov-report html --cov=chamber -p no:warnings --ignore=tests_depreciated\
 echo Testing complete.
  
