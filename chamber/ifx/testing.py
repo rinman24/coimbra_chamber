ee11802e41a037e1a0fc930390e4c26e65bafbf0
Rich Inman <rinman@cleanspark.com>
2019-05-02 20:44:10 -0700

    Adds SQLTestHelper.
    The SQLTestHelper is used to setup the test MySQL database with
    the 'chamber' schema (database). The database is created when an
    instance of the helper is created. So far the only methods
    implemented include run_script and clear_db.
    
    The only script that exists is createdb.sql. Next steps include
    adding an experiment_test_data.sql script to add some data into the
    tables.

--- chamber/ifx/testing.py
+++ chamber/ifx/testing.py
@@ -3,7 +3,7 @@
 from pathlib import Path
 
 import mysql.connector
-import pyodbc
+from mysql.connector import errorcode
 
 import chamber.ifx.configuration as config
 
@@ -13,54 +13,101 @@ class SQLTestHelper(object):
 
     def __init__(self):
         """Set up mySQL for tests."""
-        self.database = config.get_value('database', 'MySQL-Server')
+        self._database = config.get_value('database', 'MySQL-Server')
 
-        # self._create_db()
+        self._create_db()
 
-    def _create_db(self):
-        """Create the target db and then sets up the schema."""
-        self.cnx = self._create_connection('sys')
-        cursor = self.cnx.cursor()
-        # Check if the database exists; if not, create it.
-        dml = f"SELECT schema_name FROM information_schema.schemata WHERE schema_name = '{self.database}';"
-        cursor.execute(dml)
-        row = cursor.fetchone()
-        if not row:
-            ddl = f'CREATE DATABASE {self.database} DEFAULT character SET latin1;'
-            cursor.execute(ddl)
-        # Now run the scripts
-        self.cnx = self._create_connection(self.database)
-        self.run_script('clearschema.sql')
-        self.run_script('createdb.sql')
-
-    def clear_db(self):
-        """Clean up sql from tests."""
-        self.run_script('clearschema.sql')
-        self.cnx.close()
+    # ------------------------------------------------------------------------
+    # 'Public' methods: included in the API
 
     def run_script(self, script_file_name):
-        """Run a sql script against the managed test database."""
+        """
+        Run a sql script against the managed test database.
+
+        The script file must be located in the relative path
+        `chamber/tests/resources` and should include MySQL statements.
+
+        Parameters
+        ----------
+        script_file_name : str or pathlib.Path
+            Name of the sql script to run, which should have a .sql extension.
+
+        Notes
+        -----
+        Comments are allowed in the script if they begin with '--'. MySQL
+        queries are allowed to span multiple lines in the script as long as
+        they terminate with a semicolon.
+
+        Examples
+        --------
+        >>> from chamber.ifx.testing import SQLTestHelper
+        >>> helper = SQLTestHelper()
+        >>> helper.run_script('createdb.sql')
+
+        """
         cursor = self.cnx.cursor()
 
         path = Path(f'chamber/tests/resources/{script_file_name}')
+
+        statement = ''
+
         with open(path) as inserts:
             sql_script = inserts.readlines()
             for part in sql_script:
-                try:
-                    part = part.strip()
-                    if part:
-                        cursor.execute(part)
-                except Exception as e:
-                    print(e)
+                part = part.replace('\n', '')
+                if part.startswith('--'):  # ignore sql comment lies
+                    continue
+                else:
+                    statement = statement + part
+                    if statement.endswith(';'):  # try to execute
+                        try:
+                            cursor.execute(statement)
+                        except mysql.connector.Error as err:
+                            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                                print(f'Something is wrong with your user name or password: {err}')
+                            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                                print(f'Database does not exist: {err}')
+                            else:
+                                print(err)
+                        else:
+                            statement = ''
 
-    def _create_connection(self, database):
+    def clear_db(self):
+        """Clean up sql from tests."""
+        # TODO: Docsring
+        cursor = self.cnx.cursor()
+
+        cursor.execute('SET FOREIGN_KEY_CHECKS = 0;')
+        cursor.execute(f'DROP DATABASE IF EXISTS `{self._database}`;')
+        cursor.execute('SET FOREIGN_KEY_CHECKS = 1;')
+
+        self.cnx.close()
+
+    # ------------------------------------------------------------------------
+    # 'Internal' methods: not part of the API
+
+    def _create_db(self):
+        """Create the target db and then sets up the schema."""
+        cnx = self._create_connection('sys')
+        cursor = cnx.cursor()
+        database = self._database
+        cursor.execute(f'CREATE SCHEMA IF NOT EXISTS `{database}` DEFAULT CHARACTER SET utf8 ;')
+        cnx.close()
+
+        self.cnx = self._create_connection()
+        self.run_script('createdb.sql')
+
+    def _create_connection(self, database=None):
         """Create a MySQL connection from configuration settings."""
         host = config.get_value('host', 'MySQL-Server')
         user = config.get_value('user', 'MySQL-Server')
         password = config.get_value('password', 'MySQL-Server')
+        if not database:
+            database = self._database
 
         kwargs = dict(host=host, user=user, password=password, database=database)
         cnx = mysql.connector.connect(**kwargs)
+
         cnx.autocommit = True
 
         return cnx
--- chamber/tests/resources/createdb.sql
+++ chamber/tests/resources/createdb.sql
@@ -1,19 +1,121 @@
+-- MySQL Workbench Forward Engineering
+
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-CREATE SCHEMA IF NOT EXISTS `workspace` DEFAULT CHARACTER SET latin1 ;
-USE `workspace` ;
+-- -----------------------------------------------------
+-- Schema chamber
+-- -----------------------------------------------------
+USE `chamber` ;
 
-CREATE TABLE IF NOT EXISTS `workspace`.`Tube` (`TubeId` TINYINT(3) UNSIGNED NOT NULL AUTO_INCREMENT, `DiameterIn` DECIMAL(7,7) UNSIGNED NOT NULL, `DiameterOut` DECIMAL(7,7) UNSIGNED NOT NULL, `Length` DECIMAL(4,4) UNSIGNED NOT NULL, `Material` VARCHAR(50) NOT NULL, `Mass` DECIMAL(7,7) UNSIGNED NOT NULL, PRIMARY KEY (`TubeId`)) ENGINE = InnoDB AUTO_INCREMENT = 2 DEFAULT CHARACTER SET = latin1;
+-- -----------------------------------------------------
+-- Table `chamber`.`Pools`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `chamber`.`Pools` (
+  `PoolId` TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `InnerDiameter` DECIMAL(7,7) UNSIGNED NOT NULL,
+  `OutterDiameter` DECIMAL(7,7) UNSIGNED NOT NULL,
+  `Height` DECIMAL(7,7) UNSIGNED NOT NULL,
+  `Material` VARCHAR(50) NOT NULL,
+  `Mass` DECIMAL(7,7) NOT NULL,
+  PRIMARY KEY (`PoolId`),
+  UNIQUE INDEX `InnerDiameter_UNIQUE` (`InnerDiameter` ASC) VISIBLE,
+  UNIQUE INDEX `OutterDiameter_UNIQUE` (`OutterDiameter` ASC) VISIBLE,
+  UNIQUE INDEX `Height_UNIQUE` (`Height` ASC) VISIBLE,
+  UNIQUE INDEX `Material_UNIQUE` (`Material` ASC) VISIBLE)
+ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `workspace`.`Setting` (`SettingId` SMALLINT(3) UNSIGNED NOT NULL AUTO_INCREMENT, `Duty` DECIMAL(4,1) UNSIGNED NOT NULL, `IsMass` BIT(1) NOT NULL, `Pressure` MEDIUMINT(6) UNSIGNED NOT NULL, `Temperature` DECIMAL(4,1) UNSIGNED NOT NULL, `TimeStep` DECIMAL(4,2) UNSIGNED NOT NULL, `Reservoir` BIT(1) NOT NULL, `TubeId` TINYINT(3) UNSIGNED NOT NULL, PRIMARY KEY (`SettingId`, `TubeId`), INDEX `fk_Setting_Tube1_idx` (`TubeId` ASC), CONSTRAINT `fk_Setting_Tube` FOREIGN KEY (`TubeId`) REFERENCES `workspace`.`Tube` (`TubeId`) ON UPDATE CASCADE) ENGINE = InnoDB AUTO_INCREMENT = 16 DEFAULT CHARACTER SET = latin1;
 
-CREATE TABLE IF NOT EXISTS `workspace`.`Test` (`TestId` SMALLINT(3) UNSIGNED NOT NULL AUTO_INCREMENT, `Author` VARCHAR(50) NOT NULL, `DateTime` DATETIME NOT NULL, `Description` VARCHAR(1000) NOT NULL, `SettingId` SMALLINT(3) UNSIGNED NOT NULL, PRIMARY KEY (`TestId`, `SettingId`), INDEX `fk_Test_Setting_idx` (`SettingId` ASC), CONSTRAINT `fk_Test_Setting` FOREIGN KEY (`SettingId`) REFERENCES `workspace`.`Setting` (`SettingId`) ON UPDATE CASCADE) ENGINE = InnoDB AUTO_INCREMENT = 18 DEFAULT CHARACTER SET = latin1;
+-- -----------------------------------------------------
+-- Table `chamber`.`Settings`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `chamber`.`Settings` (
+  `SettingId` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `Duty` DECIMAL(4,1) UNSIGNED NOT NULL,
+  `Pressure` MEDIUMINT UNSIGNED NOT NULL,
+  `Temperature` DECIMAL(4,1) UNSIGNED NOT NULL,
+  `TimeStep` DECIMAL(4,2) UNSIGNED NOT NULL,
+  PRIMARY KEY (`SettingId`),
+  UNIQUE INDEX `Duty_UNIQUE` (`Duty` ASC) VISIBLE,
+  UNIQUE INDEX `Pressure_UNIQUE` (`Pressure` ASC) VISIBLE,
+  UNIQUE INDEX `Temperature_UNIQUE` (`Temperature` ASC) VISIBLE,
+  UNIQUE INDEX `TimeStep_UNIQUE` (`TimeStep` ASC) VISIBLE)
+ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `workspace`.`Observation` (`CapManOk` BIT(1) NOT NULL, `DewPoint` DECIMAL(5,2) UNSIGNED NOT NULL, `Idx` MEDIUMINT(6) UNSIGNED NOT NULL, `Mass` DECIMAL(7,7) UNSIGNED NULL DEFAULT NULL, `OptidewOk` BIT(1) NOT NULL, `PowOut` DECIMAL(6,4) NULL DEFAULT NULL, `PowRef` DECIMAL(6,4) NULL DEFAULT NULL, `Pressure` MEDIUMINT(6) UNSIGNED NOT NULL, `TestId` SMALLINT(3) UNSIGNED NOT NULL, PRIMARY KEY (`Idx`, `TestId`), INDEX `fk_Observation_Test_idx` (`TestId` ASC), CONSTRAINT `fk_Observation_Test` FOREIGN KEY (`TestId`) REFERENCES `workspace`.`Test` (`TestId`) ON UPDATE CASCADE) ENGINE = InnoDB DEFAULT CHARACTER SET = latin1;
 
-CREATE TABLE IF NOT EXISTS `workspace`.`TempObservation` (`ThermocoupleNum` TINYINT(2) UNSIGNED NOT NULL, `Temperature` DECIMAL(5,2) NOT NULL, `Idx` MEDIUMINT(6) UNSIGNED NOT NULL, `TestId` SMALLINT(3) UNSIGNED NOT NULL, PRIMARY KEY (`Idx`, `TestId`, `ThermocoupleNum`), CONSTRAINT `fk_TempObservation_Observation` FOREIGN KEY (`Idx` , `TestId`) REFERENCES `workspace`.`Observation` (`Idx` , `TestId`) ON UPDATE CASCADE) ENGINE = InnoDB DEFAULT CHARACTER SET = latin1;
+-- -----------------------------------------------------
+-- Table `chamber`.`Tests`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `chamber`.`Tests` (
+  `TestId` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `Author` TINYTEXT NOT NULL,
+  `DateTime` DATETIME NOT NULL,
+  `Description` TEXT NOT NULL,
+  `Pools_PoolId` TINYINT UNSIGNED NOT NULL,
+  `Settings_SettingId` SMALLINT UNSIGNED NOT NULL,
+  PRIMARY KEY (`TestId`, `Pools_PoolId`, `Settings_SettingId`),
+  UNIQUE INDEX `DateTime_UNIQUE` (`DateTime` ASC) VISIBLE,
+  INDEX `fk_Tests_Pools_idx` (`Pools_PoolId` ASC) VISIBLE,
+  INDEX `fk_Tests_Settings1_idx` (`Settings_SettingId` ASC) VISIBLE,
+  CONSTRAINT `fk_Tests_Pools`
+    FOREIGN KEY (`Pools_PoolId`)
+    REFERENCES `chamber`.`Pools` (`PoolId`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_Tests_Settings1`
+    FOREIGN KEY (`Settings_SettingId`)
+    REFERENCES `chamber`.`Settings` (`SettingId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `chamber`.`Observations`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `chamber`.`Observations` (
+  `ObservationId` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `CapManOk` BIT(1) NOT NULL,
+  `DewPoint` DECIMAL(5,2) UNSIGNED NOT NULL,
+  `Idx` MEDIUMINT UNSIGNED NOT NULL,
+  `Mass` DECIMAL(7,7) UNSIGNED NOT NULL,
+  `OptidewOk` BIT(1) NOT NULL,
+  `PowOut` DECIMAL(6,4) NULL,
+  `PowRef` DECIMAL(6,4) NULL,
+  `Pressure` MEDIUMINT UNSIGNED NOT NULL,
+  `Tests_TestId` SMALLINT UNSIGNED NOT NULL,
+  `Tests_Pools_PoolId` TINYINT UNSIGNED NOT NULL,
+  `Tests_Settings_SettingId` SMALLINT UNSIGNED NOT NULL,
+  PRIMARY KEY (`ObservationId`, `Tests_TestId`, `Tests_Pools_PoolId`, `Tests_Settings_SettingId`),
+  INDEX `fk_Observations_Tests1_idx` (`Tests_TestId` ASC, `Tests_Pools_PoolId` ASC, `Tests_Settings_SettingId` ASC) VISIBLE,
+  CONSTRAINT `fk_Observations_Tests1`
+    FOREIGN KEY (`Tests_TestId` , `Tests_Pools_PoolId` , `Tests_Settings_SettingId`)
+    REFERENCES `chamber`.`Tests` (`TestId` , `Pools_PoolId` , `Settings_SettingId`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `chamber`.`Temperatures`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `chamber`.`Temperatures` (
+  `ThermocoupleId` INT UNSIGNED NOT NULL,
+  `Temperature` DECIMAL(5,2) UNSIGNED NOT NULL,
+  `Observations_ObservationId` INT UNSIGNED NOT NULL,
+  `Observations_Tests_TestId` SMALLINT UNSIGNED NOT NULL,
+  `Observations_Tests_Pools_PoolId` TINYINT UNSIGNED NOT NULL,
+  `Observations_Tests_Settings_SettingId` SMALLINT UNSIGNED NOT NULL,
+  PRIMARY KEY (`ThermocoupleId`, `Observations_ObservationId`, `Observations_Tests_TestId`, `Observations_Tests_Pools_PoolId`, `Observations_Tests_Settings_SettingId`),
+  INDEX `fk_Temperatures_Observations1_idx` (`Observations_ObservationId` ASC, `Observations_Tests_TestId` ASC, `Observations_Tests_Pools_PoolId` ASC, `Observations_Tests_Settings_SettingId` ASC) VISIBLE,
+  CONSTRAINT `fk_Temperatures_Observations1`
+    FOREIGN KEY (`Observations_ObservationId` , `Observations_Tests_TestId` , `Observations_Tests_Pools_PoolId` , `Observations_Tests_Settings_SettingId`)
+    REFERENCES `chamber`.`Observations` (`ObservationId` , `Tests_TestId` , `Tests_Pools_PoolId` , `Tests_Settings_SettingId`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
